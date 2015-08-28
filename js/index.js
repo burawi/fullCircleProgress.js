463b20e04974bea552a60b7350600960f53b6481
burawi <burawi.ba@outlook.com>
2015-08-28 20:41:05 +0200

    onHover & hoverStyle added

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+/*var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
 document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
 try {
   var pageTracker = _gat._getTracker("UA-66863336-1");
 pageTracker._trackPageview();
 } catch(err) {}
-
+*/
 $(".logo").fullCircleProgress({
     side: 100,
     levels: 9,
